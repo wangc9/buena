9c8005767901aa263f58df3fcb5cef1e96988057
Chen Wang <wang.756090@gmail.com>
2025-12-17 13:44:25 +0800

    feat: Initial turbo setup and schema package

---
+++ packages/schema/src/schema.ts
@@ -0,0 +1,42 @@
+import z from "zod";
+
+export const PropertyReturnSchema = z.object({
+  id: z.uuid(),
+  type: z.enum(["WEG", "MV"]),
+  name: z.string().min(2),
+});
+
+export const PropertySchema = z.object({
+  ...PropertyReturnSchema.omit({ id: true }).shape,
+  manager: z.string().min(2),
+  accountant: z.string().min(2),
+  url: z.url(),
+});
+
+export const BuildingDataSchema = z.object({
+  propertyId: z.uuid(),
+  name: z.string().min(2),
+  street: z.string().min(2),
+  house: z.coerce.number().int().min(0),
+  other: z.string().min(2),
+});
+
+export const BuildingArraySchema = z.object({
+  buildings: BuildingDataSchema.array().min(1),
+});
+
+export const UnitDataSchema = z.object({
+  buildingId: z.uuid(),
+  type: z.enum(["Apartment", "Office", "Garden", "Parking"]),
+  number: z.number().int().min(0),
+  floor: z.number().int().min(-1),
+  entrance: z.string().min(2),
+  size: z.number().int().min(0),
+  ownershipShare: z.string().min(2),
+  year: z.number().int().min(0),
+  rooms: z.number().int().min(0),
+});
+
+export const UnitArraySchema = z.object({
+  units: UnitDataSchema.array().min(1),
+});
